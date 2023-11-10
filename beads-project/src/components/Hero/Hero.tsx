a147b2644ef94458549077fc9914fadbf09f50ab
unknown <rizalfauzan190@gmail.com>
2023-11-10 17:46:10 +0700

    [update terbaru]

--- beads-project/src/components/Hero/Hero.tsx
+++ beads-project/src/components/Hero/Hero.tsx
@@ -1,9 +1,27 @@
 'use client';
 
+import './hero.scss';
+import Image from 'next/image';
+
 export default function Hero() {
   return (
-    <div className="Hero">
-      
-    </div>
+    <main className="hero">
+      <section className="section-hero">  
+        <section className="img-hero">
+          <Image src="/hero.png" alt="" width={130} height={130} className="img-data"/>
+        </section>
+        <h2 className="judul-hero">
+          Find your beads <br />
+          with beads me
+        </h2>
+        <p className="p-hero">
+          The best products we have<br />
+          will make your day colorful
+        </p>
+        <section className="btn-hero">
+            <button type="submit" name="submit" className="submit">Order Now</button>
+        </section>
+      </section>
+    </main>
   )
 }
